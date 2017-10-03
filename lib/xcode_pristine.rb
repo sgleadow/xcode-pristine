131443b6051ed2e06796138faa8c36fcc8228692
Stewart Gleadow <sgleadow@gmail.com>
2017-10-03 20:42:37 +1100

    Don't use squiggly heredoc so it works with ruby 2.0.0

--- lib/xcode_pristine.rb
+++ lib/xcode_pristine.rb
@@ -37,26 +37,26 @@ module XcodePristine
       end
     end
 
-    USAGE_MESSAGE = <<~END_OUTPUT
-    Error: could not find any projects or workspaces to check from the arguments passed to xcpristine.
+    USAGE_MESSAGE = <<-END_OUTPUT
+Error: could not find any projects or workspaces to check from the arguments passed to xcpristine.
 
-    Usage:
-      xcpristine <path 1> <path 2> ... <path N>
+Usage:
+  xcpristine <path 1> <path 2> ... <path N>
 
-    Where each path can be one of:
+Where each path can be one of:
 
-      an Xcode project
-        the path to a .xcodeproj, eg. ./path/to/Example.xcodeproj
-        (checks that project for build settings)
+  an Xcode project
+    the path to a .xcodeproj, eg. ./path/to/Example.xcodeproj
+    (checks that project for build settings)
 
-      an Xcode workspace
-        the path to a .xcworkspace, eg. ./path/to/Example.xcworkspace
-        (checks each top level Xcode project file in the workspace for build settings)
+  an Xcode workspace
+    the path to a .xcworkspace, eg. ./path/to/Example.xcworkspace
+    (checks each top level Xcode project file in the workspace for build settings)
 
-      a directory
-        the path to a directory containing .xcodeproj or .xcworkspace files
-        (if found, checks the workspace, otherwise, checks the project)
-    END_OUTPUT
+  a directory
+    the path to a directory containing .xcodeproj or .xcworkspace files
+    (if found, checks the workspace, otherwise, checks the project)
+END_OUTPUT
   end
 
   class Status
--- spec/xcode_pristine_spec.rb
+++ spec/xcode_pristine_spec.rb
@@ -6,10 +6,10 @@ RSpec.describe XcodePristine do
       it "should run successfully" do
         project_dir = File.join(File.dirname(__FILE__), "examples", "SampleNoSettings")
 
-        expected_output = <<~END_OUTPUT
-        Project...
-        Project... OK.
-        END_OUTPUT
+        expected_output = <<-END_OUTPUT
+Project...
+Project... OK.
+END_OUTPUT
 
         expect { @result = XcodePristine::Runner.run [project_dir] }.to output(expected_output).to_stdout
         expect(@result).to eq XcodePristine::Status::OK
@@ -20,19 +20,19 @@ RSpec.describe XcodePristine do
       it "should detect and print those build settings" do
         project_dir = File.join(File.dirname(__FILE__), "examples", "SampleWithSettings")
 
-        expected_output = <<~END_OUTPUT
-        Project...
-          Debug: TARGETED_DEVICE_FAMILY=1
-          Release: TARGETED_DEVICE_FAMILY=2
-          Sample, Debug: IPHONEOS_DEPLOYMENT_TARGET=10.0
-          Sample, Release: IPHONEOS_DEPLOYMENT_TARGET=10.0
-          SampleTests, Debug: SDKROOT=iphoneos
-          SampleTests, Debug: SUPPORTED_PLATFORMS=iphonesimulator iphoneos
-          SampleTests, Release: SDKROOT=iphoneos
-          SampleTests, Release: SUPPORTED_PLATFORMS=iphonesimulator iphoneos
-          SampleUITests, Debug: ENABLE_TESTABILITY=YES
-        Project... Failed.
-        END_OUTPUT
+        expected_output = <<-END_OUTPUT
+Project...
+  Debug: TARGETED_DEVICE_FAMILY=1
+  Release: TARGETED_DEVICE_FAMILY=2
+  Sample, Debug: IPHONEOS_DEPLOYMENT_TARGET=10.0
+  Sample, Release: IPHONEOS_DEPLOYMENT_TARGET=10.0
+  SampleTests, Debug: SDKROOT=iphoneos
+  SampleTests, Debug: SUPPORTED_PLATFORMS=iphonesimulator iphoneos
+  SampleTests, Release: SDKROOT=iphoneos
+  SampleTests, Release: SUPPORTED_PLATFORMS=iphonesimulator iphoneos
+  SampleUITests, Debug: ENABLE_TESTABILITY=YES
+Project... Failed.
+END_OUTPUT
 
         expect { @result = XcodePristine::Runner.run [project_dir] }.to output(expected_output).to_stdout
         expect(@result).to eq XcodePristine::Status::SETTINGS
@@ -45,21 +45,21 @@ RSpec.describe XcodePristine do
       it "should detect and print those build settings" do
         project_dir = File.join(File.dirname(__FILE__), "examples", "SampleWorkspace", "Sample.xcworkspace")
 
-        expected_output = <<~END_OUTPUT
-        Project...
-        Project... OK.
-        Project...
-          Debug: TARGETED_DEVICE_FAMILY=1
-          Release: TARGETED_DEVICE_FAMILY=2
-          Sample, Debug: IPHONEOS_DEPLOYMENT_TARGET=10.0
-          Sample, Release: IPHONEOS_DEPLOYMENT_TARGET=10.0
-          SampleTests, Debug: SDKROOT=iphoneos
-          SampleTests, Debug: SUPPORTED_PLATFORMS=iphonesimulator iphoneos
-          SampleTests, Release: SDKROOT=iphoneos
-          SampleTests, Release: SUPPORTED_PLATFORMS=iphonesimulator iphoneos
-          SampleUITests, Debug: ENABLE_TESTABILITY=YES
-        Project... Failed.
-        END_OUTPUT
+        expected_output = <<-END_OUTPUT
+Project...
+Project... OK.
+Project...
+  Debug: TARGETED_DEVICE_FAMILY=1
+  Release: TARGETED_DEVICE_FAMILY=2
+  Sample, Debug: IPHONEOS_DEPLOYMENT_TARGET=10.0
+  Sample, Release: IPHONEOS_DEPLOYMENT_TARGET=10.0
+  SampleTests, Debug: SDKROOT=iphoneos
+  SampleTests, Debug: SUPPORTED_PLATFORMS=iphonesimulator iphoneos
+  SampleTests, Release: SDKROOT=iphoneos
+  SampleTests, Release: SUPPORTED_PLATFORMS=iphonesimulator iphoneos
+  SampleUITests, Debug: ENABLE_TESTABILITY=YES
+Project... Failed.
+END_OUTPUT
 
         expect { @result = XcodePristine::Runner.run [project_dir] }.to output(expected_output).to_stdout
         expect(@result).to eq XcodePristine::Status::SETTINGS
@@ -69,26 +69,26 @@ RSpec.describe XcodePristine do
 
   context "with invalid arguments" do
     before(:example) do
-      @usage_output = <<~END_OUTPUT
-      Error: could not find any projects or workspaces to check from the arguments passed to xcpristine.
+      @usage_output = <<-END_OUTPUT
+Error: could not find any projects or workspaces to check from the arguments passed to xcpristine.
 
-      Usage:
-        xcpristine <path 1> <path 2> ... <path N>
+Usage:
+  xcpristine <path 1> <path 2> ... <path N>
 
-      Where each path can be one of:
+Where each path can be one of:
 
-        an Xcode project
-          the path to a .xcodeproj, eg. ./path/to/Example.xcodeproj
-          (checks that project for build settings)
+  an Xcode project
+    the path to a .xcodeproj, eg. ./path/to/Example.xcodeproj
+    (checks that project for build settings)
 
-        an Xcode workspace
-          the path to a .xcworkspace, eg. ./path/to/Example.xcworkspace
-          (checks each top level Xcode project file in the workspace for build settings)
+  an Xcode workspace
+    the path to a .xcworkspace, eg. ./path/to/Example.xcworkspace
+    (checks each top level Xcode project file in the workspace for build settings)
 
-        a directory
-          the path to a directory containing .xcodeproj or .xcworkspace files
-          (if found, checks the workspace, otherwise, checks the project)
-      END_OUTPUT
+  a directory
+    the path to a directory containing .xcodeproj or .xcworkspace files
+    (if found, checks the workspace, otherwise, checks the project)
+END_OUTPUT
     end
 
     context "because the directory does not contain a project" do
